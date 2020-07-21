f8f118438addb7f6a38917ca4d39505488ab3e2b
Roman Drozd <homa1990@yandex.ru>
2020-07-21 17:06:15 +0300

    main page full

--- sources/src/js/_libs_concat.js
+++ sources/src/js/_libs_concat.js
@@ -1,4 +1,5 @@
 module.exports = [
     "./src/libs/jquery/dist/jquery.min.js",
-    "./src/libs/slick-carousel/slick/slick.min.js"
+    "./src/libs/rangeslider.js/dist/rangeslider.min.js",
+    "./src/libs/simplebar/dist/simplebar.min.js",
 ];
